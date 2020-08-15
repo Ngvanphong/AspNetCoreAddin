e91e4442f67302069eae26b4bd7e3aac79a40c95
Nguyen van Phong <ngvanphong2012@gmail.com>
2020-08-15 15:50:48 +0900

    90

---
+++ AspNetCoreAddin/Controllers/Components/FooterViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AspNetCoreAddin.Controllers.Components
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        public FooterViewComponent()
+        {
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return await Task.FromResult((IViewComponentResult)View());
+        }
+    }
+}
